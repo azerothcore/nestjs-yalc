5379abb96e8cb8935bee977b5196e3a5e7d92578
Yehonal <joseph@fung.money>
2024-04-13 13:49:08 +0200

    fix: security dependency

--- database/src/db-ops.service.ts
+++ database/src/db-ops.service.ts
@@ -4,13 +4,10 @@ import { Injectable, Provider } from '@nestjs/common';
 import type { LoggerService } from '@nestjs/common';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { dbConnectionMap } from './conn.helper.js';
-import * as Engine from 'typeorm-model-generator/dist/src/Engine.js';
-import * as ConnOptions from 'typeorm-model-generator/dist/src/IConnectionOptions.js';
-import { getDefaultGenerationOptions } from 'typeorm-model-generator/dist/src/IGenerationOptions.js';
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions.js';
 import { getDataSourceToken } from '@nestjs/typeorm';
 
-type IConnectionOptions = ConnOptions.default;
+
 
 /**
  * Application service
@@ -72,58 +69,6 @@ export class DbOpsService {
       this.loggerService.debug?.(`Dropping ${v.conn.driver.schema}`);
       await queryRunner.dropSchema(v.conn.driver.schema.toString(), true, true);
     }
-  }
-
-  public async generate(dbName: string, tables: string[], genPath?: string) {
-    this.loggerService.debug?.('Exporting db to TypeORM entities...');
-
-    const driver = Engine.createDriver('mysql');
-
-    const mysqlConnectionOptions: MysqlConnectionOptions[] = [];
-    this.dbConnections.forEach(({ conn: { options } }) => {
-      if (isMysqlConnectionOption(options) && dbName === options.database) {
-        mysqlConnectionOptions.push(options);
-      }
-    });
-
-    if (!mysqlConnectionOptions.length) {
-      this.loggerService.error(
-        `There is no MySQL database connection configured for ${dbName}. ` +
-          'Please refer to the documentation for Database Connection Setup',
-      );
-      return;
-    }
-
-    for (const options of mysqlConnectionOptions) {
-      const connOptions: IConnectionOptions = {
-        ...ConnOptions.getDefaultConnectionOptions(),
-        host: options.host ?? '127.0.0.1',
-        port: options.port ?? 3306,
-        password: options.password ?? '',
-        user: options.username ?? '',
-        databaseNames: options.database ? [options.database] : [],
-        databaseType: options.type,
-        onlyTables: tables,
-      };
-
-      const generationOptions = {
-        ...getDefaultGenerationOptions(),
-      };
-
-      if (genPath) {
-        generationOptions.resultsPath = genPath;
-      }
-
-      generationOptions.resultsPath += `/${dbName}`;
-
-      await Engine.createModelFromDatabase(
-        driver,
-        connOptions,
-        generationOptions,
-      );
-    }
-
-    this.loggerService.debug?.('Export complete!');
   }
 }
 
